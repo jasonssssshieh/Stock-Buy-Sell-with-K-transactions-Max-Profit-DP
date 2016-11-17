1248e8f3cc3399587655292e21e41ecc22c477b5
Jason Xie <jasonssssshieh@users.noreply.github.com>
2016-11-17 17:06:56 -0500

    Update .cpp

--- .cpp
+++ .cpp
@@ -30,18 +30,29 @@ public:
          2. 在买这个股票之前的收入，那么也就是说我们到m天为止，就一定有了i-1此交易，所以dp[i-1][m]
          for m = 0,1,2,```,j-1
          取两者最大的即可
+         这里对于时间复杂度还是可以优化，从O(p^2 * k)->O(p*k)
+         因为其实对于每一次比较，我们的没必要每次都从头开始找m，只需要把当前的最大的那个m的取值，与新加入进来的那个取值对比即可。
+         prices[j] - prices[m] + dp[i-1][m]
+         =>
+         price[j] + (dp[i-1][m] - prices[m])
          */
         int n = (int)prices.size();
         vector<vector<int>> dp(k+1,vector<int>(n,0));
         for(int i = 1; i <= k; ++i){
+            int maxDiff = dp[i-1][0] - prices[0];
             for(int j = 1; j < n; ++j){
+                dp[i][j] = max(prices[j] + maxDiff, dp[i][j-1]);
+                maxDiff = max(maxDiff, dp[i-1][j] - prices[j]);//每次循环更新一下即可。
+                /*//不做优化：
                 dp[i][j] = dp[i][j-1];
                 for(int m = 0; m < j; ++m){
                     dp[i][j] = max(dp[i][j], prices[j] - prices[m] + dp[i-1][m]);
                 }
+               */
+                 
             }
         }
-        cout<<"The max profit with k = "<<k<<" times transactions is: "<<dp[k-1][n-1]<<endl;;
+        cout<<"The max profit with k = "<<k<<" times transactions is: "<<dp[k][n-1]<<endl;;
         return dp[k][n-1];
     }
 };
